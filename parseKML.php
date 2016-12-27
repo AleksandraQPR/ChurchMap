1b3f616e1146cca5d10044d6c115f0980daeb797
Robert Szulist <rszulist@hso.grupa.skok>
2016-12-27 19:11:33 +0100

    usunieto var_dump

--- parseKML.php
+++ parseKML.php
@@ -10,17 +10,17 @@ function pKML($file) {
 
     foreach($xml->getElementsByTagName('Placemark') as $placemark) {
         foreach ( $placemark->getElementsByTagName('name') as $name) {
-            var_dump($name->nodeValue);
+            
             $output[$name->nodeValue] = array();
         }
     
         foreach ( $placemark->getElementsByTagName('description') as $desc) {
-            var_dump($desc->nodeValue);
+            
             $output[$name->nodeValue]['desc'] = $desc->nodeValue ;
         }
         
         foreach ( $placemark->getElementsByTagName('Point') as $point) {
-            var_dump($point->nodeValue);
+            
             $latlng = array();
             foreach ($point->getElementsByTagName('coordinates') as $coordinates) {
                 
