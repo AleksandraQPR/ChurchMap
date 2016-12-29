8b470b57400447febb63e92a45f1e55e6dad15f7
Robert Szulist <szulist.r@gmail.com>
2016-12-29 13:59:38 +0100

    odebrany plik jest parsowany w locie

--- odbierz.php
+++ odbierz.php
@@ -4,13 +4,18 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
+require_once 'parseKML.php';
 
 if (isset($_FILES['upload_file'])) {
-    if(move_uploaded_file($_FILES['upload_file']['tmp_name'], "tmp/" . $_FILES['upload_file']['name'])){
+
+    try {
+        $tablic = pKML($_FILES['upload_file']['tmp_name']);
         echo $_FILES['upload_file']['name']. " OK";
-    } else {
-        echo $_FILES['upload_file']['name']. " KO";
     }
+    catch (Exception $e) {
+        echo $_FILES['upload_file']['name']. " KO";
+    }    
+        var_dump($tablic);
     exit;
 } else {
     echo "No files uploaded ...";
