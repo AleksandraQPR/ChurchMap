bc778bd2390716244da2748c909f85d5671c0dac
Robert Szulist <szulist.r@gmail.com>
2017-01-13 22:39:59 +0100

    dodano dorzucanie punktow do db

--- dodaj.php
+++ dodaj.php
@@ -1,6 +1,12 @@
 <?php
 
+require_once 'dbConnect.php';
 
+$dane = $_POST;
 
+addPlacemarkToDB($dane['nazwaKosciola'],
+                $dane['adresKosciola'],
+                $dane['dlugoscGeograficzna'],
+                $dane['szerokoscGeograficzna']);
 
 ?>
