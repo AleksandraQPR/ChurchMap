4ea8f66bccf182f16b0837d889ad3a2af393b3bf
Robert Szulist <szulist.r@gmail.com>
2017-01-13 21:08:54 +0100

    integracja walidacji KML

--- parseKML.php
+++ parseKML.php
@@ -1,5 +1,7 @@
 <?php
 
+include_once 'ValidateKML.php';
+
 function pKML($file) {
     // Parsowanie kliku kml
     // Zwraca tablice asocjacyjna 
@@ -10,40 +12,46 @@ function pKML($file) {
         throw new Exception('Nie udalo sie zaladowac pliku');
     }
 
-    foreach($xml->getElementsByTagName('Placemark') as $placemark) {
-        foreach ( $placemark->getElementsByTagName('name') as $name) {
-            
-            $output[$name->nodeValue] = array();
-        }
-    
-        foreach ( $placemark->getElementsByTagName('description') as $desc) {
-            
-            $output[$name->nodeValue]['desc'] = $desc->nodeValue ;
-        }
-        
-        foreach ( $placemark->getElementsByTagName('Point') as $point) {
-            
-            $latlng = array();
-            foreach ($point->getElementsByTagName('coordinates') as $coordinates) {
+    if(validateKML($xml)) {
+
+        foreach($xml->getElementsByTagName('Placemark') as $placemark) {
+            foreach ( $placemark->getElementsByTagName('name') as $name) {
                 
-                $coordinate = $coordinates->nodeValue;
-                $coordinate = str_replace(" ", "", $coordinate);
-            
-                $tmp = explode(",", $coordinate);
-                
-                $latlng['longitude'] = doubleval($tmp[0]);
-                $latlng['latitude'] = doubleval($tmp[1]);
-                $latlng['altitude'] = doubleval($tmp[2]);
+                $output[$name->nodeValue] = array();
             }
+        
+            foreach ( $placemark->getElementsByTagName('description') as $desc) {
+                
+                $output[$name->nodeValue]['desc'] = $desc->nodeValue ;
+            }
+            
+            foreach ( $placemark->getElementsByTagName('Point') as $point) {
+                
+                $latlng = array();
+                foreach ($point->getElementsByTagName('coordinates') as $coordinates) {
+                    
+                    $coordinate = $coordinates->nodeValue;
+                    $coordinate = str_replace(" ", "", $coordinate);
+                
+                    $tmp = explode(",", $coordinate);
+                    
+                    $latlng['longitude'] = doubleval($tmp[0]);
+                    $latlng['latitude'] = doubleval($tmp[1]);
+                    $latlng['altitude'] = doubleval($tmp[2]);
+                }
 
-        $output[$name->nodeValue]['coord'] = $latlng;
+            $output[$name->nodeValue]['coord'] = $latlng;
+            }
         }
-    }
 
-// output ma nastepujaca postac:
-// $output = array('Twoj kociol' => array('desc' => 'www.kosciol.pl', 'coord'=> array('longitude'=>'dlugosc', 'latitude'=>'szerokosc', 'altitude'=>'wysokosc') ));
-    
-    return $output; 
+    // output ma nastepujaca postac:
+    // $output = array('Twoj kociol' => array('desc' => 'www.kosciol.pl', 'coord'=> array('longitude'=>'dlugosc', 'latitude'=>'szerokosc', 'altitude'=>'wysokosc') ));
+        
+        return $output;
+    }
+    else {
+        return false;
+    }
 }
 
 ?>
