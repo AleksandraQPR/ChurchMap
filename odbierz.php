5bad7d7df95121e54840a48ea52c009355c26be6
Robert Szulist <szulist.r@gmail.com>
2017-01-13 21:15:07 +0100

    punkty dodawane do mapy po uploadzie pliku

--- odbierz.php
+++ odbierz.php
@@ -5,11 +5,13 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 
 require_once 'parseKML.php';
+require_once 'dbConnect.php';
 
 if (isset($_FILES['upload_file'])) {
 
     try {
         $tablic = pKML($_FILES['upload_file']['tmp_name']);
+        addPlacemarksToDB($tablic);
         echo $_FILES['upload_file']['name']. " OK";
     }
     catch (Exception $e) {
