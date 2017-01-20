342b1525e89cdc9619f7319ddd7ced0081fa5909
DESKTOP-FGJKRN5\Maciej <kali6@wp.pl>
2017-01-20 13:57:11 +0100

    zgurpowanie funkcji do jednej lini

--- create.sql
+++ create.sql
@@ -2,17 +2,10 @@ CREATE DATABASE koscioly;
 USE koscioly;
 
 CREATE TABLE `churches` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `description` text CHARACTER SET utf8 COLLATE utf8_polish_ci NOT NULL,
   `longitude` float NOT NULL,
   `latitude` float NOT NULL,
   `altitude` float NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
-ALTER TABLE `churches`
-  ADD PRIMARY KEY (`id`);
-
-ALTER TABLE `churches`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT,
