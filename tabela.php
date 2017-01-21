4dbf1e8e7f4a515788db9469663b3e995801b31b
Robert Szulist <szulist.r@gmail.com>
2017-01-21 22:05:23 +0100

    standardowa formatka html

--- tabela.php
+++ tabela.php
@@ -1,24 +1,37 @@
 <!DOCTYPE html>
-<table>
-<?php
+<html lang="pl">
+    <head>
+        <meta charset="utf-8" />
+		<title>Mapa kościołów</title>
+		<link href="style.css" rel="stylesheet" type="text/css" />
+		<link href="https://fonts.googleapis.com/css?family=Libre+Franklin" rel="stylesheet">
+        <script type="text/javascript"
+        src="http://maps.googleapis.com/maps/api/js?sensor=false"></script>
+        <script src="mapaFunctions.js"></script>
+    </head>
+    <body>
+        <table class="kontenerDAnych">
+        <?php
 
-require_once 'dbConnect.php';
+        require_once 'dbConnect.php';
 
-$kosc = getAllPlacemarksFromDB();
+        $kosc = getAllPlacemarksFromDB();
 
-foreach ($kosc as $k => $v) {
-    $a= $v['desc'];
+        foreach ($kosc as $k => $v) {
+            $a= $v['desc'];
 
-$row = <<<HTML
-    <tr>
-        <th>$k</th>
-        <th>$a</th>
-        <th><!-- miejsce na guzik usuwania --></th>
-    </tr>
+        $row = <<<HTML
+            <tr>
+                <th>$k</th>
+                <th>$a</th>
+                <th><!-- miejsce na guzik usuwania --></th>
+            </tr>
 HTML;
 
-    echo $row;
-}
+            echo $row;
+        }
 
-?>
-</table>
+        ?>
+    </table>
+    </body>
+</html>
