77d919164b3b14bc3f3deda8e1e0f2a7f0120160
Robert Szulist <szulist.r@gmail.com>
2017-01-21 22:02:54 +0100

    wyswietlanie niesformatowanej tabeli

--- tabela.php
+++ tabela.php
@@ -1,3 +1,24 @@
+<!DOCTYPE html>
+<table>
 <?php
 
+require_once 'dbConnect.php';
+
+$kosc = getAllPlacemarksFromDB();
+
+foreach ($kosc as $k => $v) {
+    $a= $v['desc'];
+
+$row = <<<HTML
+    <tr>
+        <th>$k</th>
+        <th>$a</th>
+        <th><!-- miejsce na guzik usuwania --></th>
+    </tr>
+HTML;
+
+    echo $row;
+}
+
 ?>
+</table>
