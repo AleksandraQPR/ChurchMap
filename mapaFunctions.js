d881240b5017831f845818a03c24287da3172931
Robert Szulist <szulist.r@gmail.com>
2017-01-30 20:52:33 +0100

    dodano odświeżanie punktów po imporcie KML

--- mapaFunctions.js
+++ mapaFunctions.js
@@ -139,6 +139,7 @@ function upload() {
             if (xhr.readyState == 4 && xhr.status == 200) {
                 
                 console.log(xhr.responseText); // handle response.
+                retrieve();
             }
         };
         fd.append("upload_file", file);
